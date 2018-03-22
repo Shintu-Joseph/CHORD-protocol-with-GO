a2defed258b5a3e8735ca187cf1d21339d10f9e2
Shintu Joseph <shintu.joseph@umbc.edu>
2018-03-21 21:22:23 -0400

    Added hash function

--- chord-hash.go
+++ chord-hash.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"hash/crc32"
+	"math/rand"
+)
 
-func hashID() {
-	fmt.Println("in hash func")
+func hashID(ringOrder int) uint64 {
+	generateRandomID(40)
+	return 0
+}
+
+func generateRandomID(size int) {
+	nodeList := [20]uint32{}
+	for i := 0; i < len(nodeList); i++ {
+		nodeList[i] = hasKey(randString())
+	}
+	fmt.Println(nodeList)
+}
+
+var alphabets = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString() string {
+	b := make([]rune, 15)
+	for i := range b {
+		b[i] = alphabets[rand.Intn(52)]
+	}
+	return string(b)
+}
+
+func hasKey(obj string) uint32 {
+	var scratch [64]byte
+	if len(obj) < 64 {
+
+		copy(scratch[:], obj)
+	}
+	return crc32.ChecksumIEEE(scratch[:len(obj)])
 }
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"strconv"
+)
+
 func main() {
-	hashID()
+	//scanner := bufio.NewScanner(os.Stdin)
+	//fmt.Println("Enter the order of the node size on powers of 2:")
+	//scanner.Scan()
+	ringOrder, _ := strconv.Atoi("32")
+	hashID(ringOrder)
 }
